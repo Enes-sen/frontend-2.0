b1020894fed800c43ff4dd399fac059ea0e7b5c5
darklord <113107334+Enes-sen@users.noreply.github.com>
2023-03-15 16:06:04 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "alertifyjs/build/css/alertify.min.css";
 import { Provider } from "react-redux";
 import store from "../src/redux/reducers/config";
-import { BrowserRouter} from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <BrowserRouter basename="/posts">
+  <HashRouter">
     <Provider store={store}>
       <App />
     </Provider>
