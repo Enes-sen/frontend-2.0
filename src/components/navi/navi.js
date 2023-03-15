2da7ffa16d10f9a0d75f68aa67a8faae62c87e58
darklord <113107334+Enes-sen@users.noreply.github.com>
2023-03-15 19:08:12 +0300

    Update navi.js

--- src/components/navi/navi.js
+++ src/components/navi/navi.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { Navbar, Nav, NavbarToggler, NavbarBrand, Collapse} from "reactstrap";
-import brand from"../../images/ses_logo.png";
+import brandLogo from "../../images/ses_logo.png";
 
 export default class Navi extends Component {
   constructor(props) {
@@ -21,7 +21,9 @@ export default class Navi extends Component {
   render() {
     return (
       <Navbar style={{backgroundColor: "rgb(255, 165, 0)",color:"white"}} expand="md" fixed="top" className="mb-3">
-        <NavbarBrand tag={Link} to="/posts">{brand}</NavbarBrand>
+        <NavbarBrand tag={Link} to="/">
+          <img src={brandLogo} alt="Brand logo" width="100" height="100" />
+        </NavbarBrand>
         <NavbarToggler onClick={this.toggle} />
         <Collapse isOpen={this.state.isOpen} navbar>
         </Collapse>
