1a873f911e588149ce9422b4b3f857ab016a2eaf
darklord <113107334+Enes-sen@users.noreply.github.com>
2023-03-15 15:39:58 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,17 +5,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "alertifyjs/build/css/alertify.min.css";
 import { Provider } from "react-redux";
 import store from "../src/redux/reducers/config";
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import { BrowserRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <BrowserRouter>
     <Provider store={store}>
-      <Routes>
-        <Route exact path="*" element={<App />} />
-        <Route path="/" element={<Navigate from="/" to="/posts" />} />
-      </Routes>
+      <App />
     </Provider>
   </BrowserRouter>
 );
